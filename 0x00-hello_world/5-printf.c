ca8abe9fcdd53adc5813d6ceb8331d348306558d
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-07 18:34:20 +0200

    5-pirntf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: This is a C program that prints a message
+ * Description: 'C program for print'
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
