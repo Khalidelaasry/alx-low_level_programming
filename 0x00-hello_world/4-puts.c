35bf430e0101067d75559019b3498daf2d6fc2c6
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-07 20:50:22 +0200

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/**
+/*
  * main - Entry point
- * Description: 'the program's description'
+ *
+ * Description: This C program prints the sizes of different data types
+ *              (char, int, long int, long long int, float) on the current system.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
