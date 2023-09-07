80da80f33271cc111a96242f7a4f17173f8e9bcc
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-07 18:26:29 +0200

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: This is a C program that prints a message
+ *              with proper grammar, creating a piece of art.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
