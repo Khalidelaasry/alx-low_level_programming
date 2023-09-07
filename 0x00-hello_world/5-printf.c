5c49fb6303eb60889ce574aef7f8e865c481089e
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-07 18:29:36 +0200

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  *              with proper grammar, creating a piece of art.
  * Return: Always 0 (Success)
  */
-int main(void)
+int	main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return 0;
