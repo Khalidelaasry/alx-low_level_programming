5e231c838ba7fd3342fa4001868a2419e48deffd
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-08 18:13:14 +0200

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 /*
+ * main - Entry point
  * This C program prints the lowercase alphabet in a single line
  * using a for loop. It starts from 'a' to 'z', printing each
  * character using putchar. After printing the alphabet.
