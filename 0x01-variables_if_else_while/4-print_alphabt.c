a9392f7b44f74ec04a50e048050e8c27ccb4265f
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-08 18:39:57 +0200

    4. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,8 +3,8 @@
  * main - Entry point
  *
  * Description: This C program prints the lowercase alphabet in a single line
- * excluding the letters 'e' and 'q' using a for loop. It starts from 'a' and
- * iterates up to 'z', printing each character using putchar if it's not 'e' or 'q'.
+ * excluding the letters 'e' and 'q' using a for loop. It starts from 'a'
+ * to 'z', printing each character using putchar if it's not 'e' or 'q'.
  * After printing the modified alphabet, it inserts a newline.
  *
  * Return: Always 0 (Success)
@@ -12,7 +12,6 @@
 int main(void)
 {
 	char letter;
-	
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')
