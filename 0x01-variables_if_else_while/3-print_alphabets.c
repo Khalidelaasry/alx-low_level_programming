b25c43385e14cec5de514d7fc5318bee50b3a8b8
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-08 18:22:05 +0200

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
-
+/*
+ * This C program prints the lowercase and uppercase English alphabet in a single
+ * line using two for loops. The first loop prints lowercase letters from 'a' to 'z',
+ * and the second loop prints uppercase letters from 'A' to 'Z'. After printing both
+ * alphabets, it inserts a newline.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char lowercase;
-	char uppercase;
-	
+	char uppercase;	
 	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 	{
 		putchar(lowercase);
 	}
-	
 	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
 	{
 		putchar(uppercase);
 	}
-	
 	putchar('\n');
 	
 	return 0;
