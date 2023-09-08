b909fcf9559565d73e09683e57753d9d6f90ccdd
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-08 19:54:14 +0200

    6-numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * Prints digits from 0 to 9 using ASCII values and putchar.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
