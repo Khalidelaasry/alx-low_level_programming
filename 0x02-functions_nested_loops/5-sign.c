b8f595745a246887070d71f022f8eabe8750cf54
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-12 18:23:35 +0200

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,16 @@
 #include <unistd.h>
 #include "main.h"
-
+/*
+ * print_sign - Prints the sign of a number
+ *
+ * @n: The number to check
+ *
+ * Return: 1 if 'n' is positive, -1 if 'n' is negative, 0 if 'n' is zero
+ *
+ * Description: This function takes an integer 'n' as input and checks its sign.
+ *              It then prints the sign ('+', '-' or '0') using _putchar, and
+ *              returns 1 if 'n' is positive, -1 if 'n' is negative, and 0 if 'n' is zero.
+ */
 int print_sign(int n)
 {
 	if (n > 0)
@@ -10,12 +20,12 @@ int print_sign(int n)
 	}
 	else if (n < 0)
 	{
-		  _putchar('-');
-                return (-1);
+		_putchar('-');
+		return (-1);
 	}
 	else
 	{
-		  _putchar('0');
-                return (0);
+		_putchar('0');
+		return (0);
 	}
 }
