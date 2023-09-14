162dcf039821fc60cbd48bf738b52f9d31090c95
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-14 17:41:01 +0200

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * print_numbers - Prints the digits from 0 to 9
+ *
+ * Description:This function uses a loop to iterate through digits from 0 to 9,
+ * and prints each digit using the _putchar function. It ends with a newline.
+ */
 
 void print_numbers(void)
 {
