5822a40380d45e502f6f26e5faf04045bcad5d12
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-14 18:50:32 +0200

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * Return:0
+ */
+
+void print_numbers(void)
+{
+	char i = 0;
+
+	while (i <= 9)
+	{
+		_putchar('0' + i);
+		i++;
+
+	}
+	_putchar('\n');
+
+}
