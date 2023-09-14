0a8ef4c1045eb738d6fd0fcfec49b4038d0a3b68
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-14 17:42:09 +0200

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,15 @@
  * Description:This function uses a loop to iterate through digits from 0 to 9,
  * and prints each digit using the _putchar function. It ends with a newline.
  */
-
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= '9'; i++)
+	i = 0;
+	while (i < 10)
 	{
 		_putchar(i + '0');
+		i++;
 	}
 	_putchar('\n');
 }
