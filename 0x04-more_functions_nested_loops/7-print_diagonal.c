0c14003a4333729ec8d1714935d305a36fb046a6
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-14 20:32:04 +0200

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,21 @@
 #include "main.h"
 
 /**
- * print_diagonal - Draws a diagonal line using the \ character.
+ * print_line - Draws a straight line using the character _.
  *
- * @n: The number of \ characters to be printed.
+ * @n: The number of _ characters to be printed.
  */
-void print_diagonal(int n)
+
+void print_line(int n)
 {
+	int i;
+
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < i; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
+		for (i = 1; i <= n; i++)
+			_putchar('_');
+		_putchar('\n');
 	}
 }
