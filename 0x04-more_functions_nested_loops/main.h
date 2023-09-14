cc80d82405f1203488befc0d17d571ff073953bd
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-14 18:12:13 +0200

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include<stdio.h>
+#include <stdio.h>
 
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 
-#endif /* MAIN_H */
+#endif
