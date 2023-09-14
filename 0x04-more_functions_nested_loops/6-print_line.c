3f17d85f83716e96146742a43cbd2e12d42d8563
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-14 19:59:57 +0200

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,11 @@
 #include "main.h"
-/*
+/**
  * print_line - Prints a line of underscores
  *
  * @n: The number of underscores to print
  *
  * Description: This function prints a line consisting of 'n' underscores.
- *              If 'n' is less than or equal to zero, it simply prints a newline.
+ * If 'n' is less than or equal to zero, it simply prints a newline.
  */
 void print_line(int n)
 {
