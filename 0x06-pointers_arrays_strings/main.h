84e1428c1981a64cc281df6f831aa7f12dd51eed
khalidelaasry <khalidelaasry22@gmail.com>
2023-09-20 22:29:20 +0200

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+#ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
 
@@ -14,5 +15,4 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
 #endif
