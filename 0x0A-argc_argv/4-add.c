f0dc061df793bf9264ae8263ca4508f0a7946b15
khalidelaasry <khalidelaasry22@gmail.com>
2023-10-02 17:23:03 +0200

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of the program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of strings representing the arguments.
+ *
+ * Return: 0 if successful, 1 if an error occurred.
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		int num = atoi(argv[i]);
+
+		if (num == 0 && *argv[i] != '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		if (num < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += num;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
