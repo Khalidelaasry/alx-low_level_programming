61b90a1984ab45a2843b5cc09cd4b824734bd050
khalidelaasry <khalidelaasry22@gmail.com>
2023-10-02 17:17:39 +0200

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,21 +5,20 @@
  * @argc: Number of command-line arguments.
  * @argv: Array of strings representing the arguments.
  *
- * Return: 1 if there are not exactly 3 arguments, otherwise return the product
- * of the two provided numbers.
+ * Return: 0 if exactly 2 arguments are provided,otherwise return an error msg.
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	if (argc != 3)
+	int i;
+	int j;
+
+	if (argc == 3)
 	{
-		printf("Error\n");
-		return (1);
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		printf("%d\n", i * j);
 	}
-
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-
-	printf("%d\n", num1 * num2);
-
+	else
+		printf("Error\n");
 	return (0);
 }
